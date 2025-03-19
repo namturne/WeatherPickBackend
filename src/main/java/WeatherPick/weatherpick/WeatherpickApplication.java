7fb7c08c5615444cd074b8f63e7256e35435cb94
tjddn <sungwk100@gmail.com>
2025-03-20 03:22:26 +0900

    test

--- src/main/java/WeatherPick/weatherpick/WeatherpickApplication.java
+++ src/main/java/WeatherPick/weatherpick/WeatherpickApplication.java
@@ -8,6 +8,7 @@ public class WeatherpickApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherpickApplication.class, args);
+		System.out.println("test");
 	}
 
 }
