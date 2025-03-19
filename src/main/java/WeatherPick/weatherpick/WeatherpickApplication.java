939b6b34313256e680fddadaf187860df9f9b265
tjddn <sungwk100@gmail.com>
2025-03-20 03:23:59 +0900

    원상복구

--- src/main/java/WeatherPick/weatherpick/WeatherpickApplication.java
+++ src/main/java/WeatherPick/weatherpick/WeatherpickApplication.java
@@ -8,7 +8,6 @@ public class WeatherpickApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherpickApplication.class, args);
-		System.out.println("test");
 	}
 
 }
