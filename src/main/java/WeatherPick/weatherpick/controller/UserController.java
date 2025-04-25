c3f83b0ee7c413adf7e70efe3a90c6168595fd7f
namturne <vivi1318@naver.com>
2025-04-26 00:57:13 +0900

    #5 의진 - Rest변환

--- src/main/java/WeatherPick/weatherpick/controller/UserController.java
+++ src/main/java/WeatherPick/weatherpick/controller/UserController.java
@@ -9,11 +9,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Collections;
 import java.util.Map;
 import WeatherPick.weatherpick.domain.user.repository.UserRepository;
